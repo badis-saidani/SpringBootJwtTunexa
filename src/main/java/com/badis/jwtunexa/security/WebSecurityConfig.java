30bdf3d5f4738a07ccfb285e396421d44d56cc9f
Badis Saidani <sa.badis@dlines.local>
2020-05-02 14:56:30 +0100

    allowed api/auth/

--- src/main/java/com/badis/jwtunexa/security/WebSecurityConfig.java
+++ src/main/java/com/badis/jwtunexa/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                //.antMatchers("/**").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/allowed/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
